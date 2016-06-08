52d849aeda038c47e4605bc227e1b5b350c0c7d8
Cleber Campanel <cleber.campanel@gmail.com>
2016-06-08 14:49:57 -0300

    variables

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -12,6 +12,8 @@ import java.text.NumberFormat;
  */
 public class MainActivity extends AppCompatActivity {
 
+    int quantity = 2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,24 +21,27 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * This method is called when the order button is clicked.
+     * This method is called when the increment button is clicked.
      */
-    public void submitOrder(View view) {
-        int quantity = 5;
-        display(quantity);
-        displayPrice(quantity * 5);
-    }
-
     public void increment(View view){
-        int quantity = 1;
         quantity = quantity + 1;
         display(quantity);
     }
 
+    /**
+     * This method is called when the decrement button is clicked.
+     */
     public void decrement(View view){
-        int quantity = 2;
         quantity = quantity - 1;
         display(quantity);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void submitOrder(View view) {
+        display(quantity);
+        displayPrice(quantity * 5);
     }
 
     /**
