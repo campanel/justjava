b9bd975455fde69fb450c00b9a739e3056064fb2
campanel <cleber.campanel@gmail.com>
2016-06-15 01:29:52 -0300

    em casa

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -2,9 +2,11 @@ package com.example.android.justjava;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
 import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 /**
@@ -40,6 +42,10 @@ public class MainActivity extends AppCompatActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
+        EditText editTextName = (EditText) findViewById(R.id.input_name);
+        String name = editTextName.getText().toString();
+        Log.v("MainActivity","Name: " + name);
+
         CheckBox checkBoxCream = (CheckBox) findViewById(R.id.checkbox_cream);
         boolean checkCream = checkBoxCream.isChecked();
         Log.v("MainActivity","Cream: " + checkCream);
@@ -49,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
         Log.v("MainActivity","Choco: " + checkChoco);
 
         int price = calculatePrice();
-        String princeMessage = createOrderSummary(price, checkCream, checkChoco);
+        String princeMessage = createOrderSummary(name, price, checkCream, checkChoco);
         displayMessage(princeMessage);
     }
 
@@ -67,8 +73,8 @@ public class MainActivity extends AppCompatActivity {
      *
      * @return mensage summary
      */
-    private String createOrderSummary(int price, boolean checkCream, boolean checkChoco) {
-        String message = "Name: Campanel";
+    private String createOrderSummary(String name , int price, boolean checkCream, boolean checkChoco) {
+        String message = "Name: " + name;
         message += "\nAdd whipped cream? " + checkCream;
         message += "\nAdd chocolate? " + checkChoco;
         message += "\nQuantity: "+ quantity;
